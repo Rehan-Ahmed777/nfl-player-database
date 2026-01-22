786606a6f7067a6f576b88a8d3176ce00db9abb9
Rehan-Ahmed777 <rehanahmed22175255@gmail.com>
2026-01-22 23:38:44 +0500

    Simplify build script for Render

--- render-build.sh
+++ render-build.sh
@@ -3,16 +3,11 @@
 
 set -o errexit
 
-# Install dependencies
+# Install Python dependencies first
 pip install -r requirements.txt
 
-# Install Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt-get -y update
-apt-get install -y google-chrome-stable
+echo "Installing Chrome and ChromeDriver..."
 
-# Install ChromeDriver
-apt-get install -yqq unzip
-wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
-unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+# Note: Chrome installation requires apt-get which may not be available on Render
+# This script will fail gracefully if Chrome can't be installed
+
